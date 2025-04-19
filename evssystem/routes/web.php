3d8b4d9d21326741ece67856015c7e38a4c2a901
asadmukhtarr <axad03213@gmail.com>
2025-04-19 20:40:37 +0500

    laravel db configration

--- evssystem/routes/web.php
+++ evssystem/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test',function(){
+    return "Hello World";
+});
+Route::get('*',function(){
+    abort(404);
+});
