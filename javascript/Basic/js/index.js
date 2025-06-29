94d6fc8d3cfc0b97d4429b289a9dae0296755b1a
asadmukhtarr <axad03213@gmail.com>
2025-06-29 20:55:43 +0500

    js basic first lecture

---
+++ javascript/Basic/js/index.js
@@ -0,0 +1,10 @@
+function abc(v) {
+    document.getElementById("result").value = v;
+}
+var name = "Asad";
+var name2 = "Musa";
+var x = 2332;
+var y = 8;
+console.log(x + y);
+/// web page is called document ..
+// Element means that tag of html ..
