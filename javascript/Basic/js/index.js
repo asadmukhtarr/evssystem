9a0c55d22a9483faa508897b8092632708074c1c
asadmukhtarr <axad03213@gmail.com>
2025-07-04 20:44:30 +0500

    calculator

--- javascript/Basic/js/index.js
+++ javascript/Basic/js/index.js
@@ -1,5 +1,14 @@
 function abc(v) {
-    document.getElementById("result").value = v;
+    if (v == "c") {
+        document.getElementById("result").value = null;
+    } else if (v == "=") {
+        var already_value = document.getElementById("result").value;
+        document.getElementById("result").value = eval(already_value);
+    } else {
+        var already_value = document.getElementById("result").value;
+        var current_value = already_value + v;
+        document.getElementById("result").value = current_value;
+    }
 }
 var name = "Asad";
 var name2 = "Musa";
